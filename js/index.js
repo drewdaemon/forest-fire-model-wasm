49542bd20c3836e6cb5e442eccf6c6d4f7a62df6
Andrew Tate <drewctate@gmail.com>
2019-10-08 20:13:09 -0600

    Semi-functional forest fire simulation in Rust

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
-import("../pkg/index.js").catch(console.error);
+async function init () {
+  const module = await import('../pkg/index.js');
+  const width = 100, height = 50;
+
+  const forest = module.Forest.new(width, height, .02, .00001);
+
+  const pre = document.getElementById("forest-canvas");
+
+  // const patchesPtr = forest.patches();
+
+  // const memory = (await import('../pkg/index_bg.wasm')).memory;
+  // const patches = new Uint8Array(memory.buffer, patchesPtr, width * height);
+
+
+  const renderLoop = () => {
+    pre.textContent = forest.render();
+    forest.tick();
+
+    requestAnimationFrame(renderLoop);
+  };
+
+  requestAnimationFrame(renderLoop);
+}
+
+init();
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use wasm_bindgen::prelude::*;
 use web_sys::console;
-
+use rand::Rng;
+use std::fmt;
 
 // When the `wee_alloc` feature is enabled, this uses `wee_alloc` as the global
 // allocator.
@@ -10,6 +11,144 @@ use web_sys::console;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+#[wasm_bindgen]
+#[repr(u8)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub enum State {
+    Tree = 0,
+    Burning = 1,
+    Empty = 2
+}
+
+impl fmt::Display for State {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self)
+        // or, alternatively:
+        // fmt::Debug::fmt(self, f)
+    }
+}
+
+#[wasm_bindgen]
+pub struct Forest {
+    width: u32,
+    height: u32,
+    p: f32,
+    f: f32,
+    patches: Vec<State>,
+}
+
+impl fmt::Display for Forest {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        for line in self.patches.as_slice().chunks(self.width as usize) {
+            for &patch in line {
+                let symbol = match patch {
+                  State::Tree => 'T',
+                  State::Burning => 'B',
+                  State::Empty => 'E',
+                };
+                write!(f, "{}", symbol)?;
+            }
+            write!(f, "\n")?;
+        }
+
+        Ok(())
+    }
+}
+
+#[wasm_bindgen]
+impl Forest {
+    fn get_index(&self, row: u32, column: u32) -> usize {
+        (row * self.width + column) as usize
+    }
+
+    fn has_burning_neighbor(&self, row: u32, column: u32) -> bool {
+        for delta_row in [self.height - 1, 0, 1].iter().cloned() {
+            for delta_col in [self.width - 1, 0, 1].iter().cloned() {
+                if delta_row == 0 && delta_col == 0 {
+                    continue;
+                }
+
+                let neighbor_row = (row + delta_row) % self.height;
+                let neighbor_col = (column + delta_col) % self.width;
+                let idx = self.get_index(neighbor_row, neighbor_col);
+                if self.patches[idx] == State::Burning {
+                  return true;
+                }
+            }
+        }
+        false
+    }
+
+    fn lightning(&self) -> bool {
+      // let mut rng = rand::thread_rng();
+      // rng.gen_range(0.0, 1.0) < self.f
+      true
+    }
+
+    fn regenerate(&self) -> bool {
+      // let mut rng = rand::thread_rng();
+      // rng.gen_range(0.0, 1.0) < self.p
+      true
+    }
+
+    pub fn tick(&mut self) {
+      let mut next = self.patches.clone();
+
+      for row in 0..self.height {
+        for col in 0..self.width {
+          let idx = self.get_index(row, col);
+          let patch_state = self.patches[idx];
+          let burning_neighbor = self.has_burning_neighbor(row, col);
+          let lightning = self.lightning();
+          let regenerate = self.regenerate();
+
+
+          let next_state = match (patch_state, burning_neighbor, lightning, regenerate) {
+            (State::Tree, _, true, _) => State::Burning,
+            (State::Tree, true, _, _) => State::Burning,
+            (State::Burning, _, _, _) => State::Empty,
+            (State::Empty, _, _, true) => State::Tree,
+            (otherwise, _, _, _) => otherwise,
+          };
+
+          console::log_1(&JsValue::from_str(&next_state.to_string()));
+          next[idx] = next_state;
+        }
+      }
+
+      self.patches = next;
+    }
+
+    pub fn new(width: u32, height: u32, p: f32, f: f32) -> Forest {
+        let patches = (0..width * height)
+            .map(|_| State::Tree)
+            .collect();
+
+        Forest {
+            width,
+            height,
+            p,
+            f,
+            patches,
+        }
+    }
+
+    pub fn width(&self) -> u32 {
+        self.width
+    }
+
+    pub fn height(&self) -> u32 {
+        self.height
+    }
+
+    pub fn render(&self) -> String {
+      self.to_string()
+    }
+
+    pub fn patches(&self) -> *const State {
+        self.patches.as_ptr()
+    }
+}
 
 // This is like the `main` function, except for JavaScript.
 #[wasm_bindgen(start)]
@@ -20,8 +159,11 @@ pub fn main_js() -> Result<(), JsValue> {
     console_error_panic_hook::set_once();
 
 
-    // Your code goes here!
-    console::log_1(&JsValue::from_str("Hello world!"));
+    Ok(())
+}
+
+#[wasm_bindgen]
+pub fn init() -> Result<(), JsValue> {
 
     Ok(())
 }
